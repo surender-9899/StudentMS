43157aebaefffd15d1468de5cd6230bbead89ea2
surender kumar mahawar <surendermahawar2@gmail.com>
2025-06-16 15:52:54 +0530

    Rename StudentMS to student-management-backend

--- StudentMS/src/main/java/com/StudentManagmentSystem/studentms/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.StudentManagmentSystem.studentms.repository;
-
-import com.StudentManagmentSystem.studentms.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
